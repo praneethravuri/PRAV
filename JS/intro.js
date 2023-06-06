03f570cc55033b3c57588d2039fc9e6ff12f23b2
Praneeth Ravuri <prav2500@gmail.com>
2023-06-06 08:05:00 +0530

    Fade in, fade out effect on intro

---
+++ JS/intro.js
@@ -0,0 +1,19 @@
+let introID = document.getElementById("intros");
+let intro1 = "<p>Hello!</p><p>I'm <span class='turq-color'>Praneeth Ravuri</span></p>"
+let intro2 = "<p>Computer Science Student</p><p>@ <span class='turq-color'>George Mason University</span></p>";
+let isIntro1 = true;
+
+function keepFading() {
+  $("#introduction").fadeOut(3000, function() {
+    if (isIntro1) {
+      introID.innerHTML = intro2;
+      isIntro1 = false;
+    } else {
+      introID.innerHTML = intro1;
+      isIntro1 = true;
+    }
+    $("#introduction").fadeIn(2500, keepFading);
+  });
+}
+
+keepFading();
