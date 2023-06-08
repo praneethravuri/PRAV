3df994c60f9922ceb84e6a38d0d42c7d010da8e0
Praneeth Ravuri <prav2500@gmail.com>
2023-06-08 11:44:45 +0530

    Update skillsTools.js

--- JS/skillsTools.js
+++ JS/skillsTools.js
@@ -1,4 +1,4 @@
-const skillsToolsList = [
+let skillsToolsList = [
   "Python",
   "C",
   "C++",
@@ -21,27 +21,28 @@ const skillsToolsList = [
   "Zorba"
 ];
 
-const skillsDiv = document.getElementById("skills-div");
-const ulCount = Math.ceil(skillsToolsList.length / 4); // Calculate the number of <ul> tags needed
+let skillsDiv = document.getElementById("skills-div");
 
-const appendChild = skillsDiv.appendChild; // Cache the appendChild method
+let ulCount = Math.ceil(skillsToolsList.length / 4); // Calculate the number of <ul> tags needed
 
-skillsToolsList.forEach((skill, index) => {
-  const skillsList = document.createElement("ul");
-  skillsList.id = `skills-list-${index + 1}`; // Assign a unique ID to each <ul> tag
-  appendChild.call(skillsDiv, skillsList); // Use the cached appendChild method
+for (let i = 0; i < skillsToolsList.length; i += 1) {
+  let skillsList = document.createElement("ul");
+  skillsList.id = "skills-list"; // Assign a unique ID to each <ul> tag
+  skillsDiv.appendChild(skillsList);
 
-  const listItem = document.createElement("li");
-  listItem.className = "skill";
+  for (let j = i * 4; j < i * 4 + 4 && j < skillsToolsList.length; j++) {
+    let listItem = document.createElement("li");
+    listItem.className = "skill";
 
-  const img = document.createElement("img");
-  img.src = `./Images/Skills-Tools-Images/${skill}.svg`; // Replace with the actual image path
-  img.alt = "";
-  listItem.appendChild(img);
+    let img = document.createElement("img");
+    img.src = `./Images/Skills-Tools-Images/${skillsToolsList[j]}.svg`; // Replace with the actual image path
+    img.alt = "";
+    listItem.appendChild(img);
 
-  const span = document.createElement("span");
-  span.innerText = skill;
-  listItem.appendChild(span);
+    let span = document.createElement("span");
+    span.innerText = skillsToolsList[j];
+    listItem.appendChild(span);
 
-  skillsList.appendChild(listItem);
-});
+    skillsList.appendChild(listItem);
+  }
+}
